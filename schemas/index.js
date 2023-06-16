9ed8289736bb9ae965cd389033f3c3c44745e330
yeonoh <beky010101@gmail.com>
2023-06-16 15:43:37 +0900

    코드 설명 주석 last commit

--- schemas/index.js
+++ schemas/index.js
@@ -1,10 +1,10 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); // mongoose module을 가져온다.
 
 // MongoDB에 연결하는 함수
 const connect = () => {
-  // mongoose를 사용하여 MongoDB에 연결합니다.
+  // mongoose를 사용하여 MongoDB에 연결한다.
   mongoose
-    .connect("mongodb://127.0.0.1:27017/nodeSoloProj")
+    .connect("mongodb://127.0.0.1:27017/nodeSoloProj") // mongoose.connect를 사용하여 MongoDB에 연결한다.
     .catch((err) => console.log(err));
 };
 
@@ -13,5 +13,5 @@ mongoose.connection.on("error", (err) => {
   console.error("몽고디비 연결 에러", err);
 });
 
-// connect 함수를 모듈의 기본 내보내기로 설정하여 다른 파일에서 사용할 수 있게 합니다.
+// connect 함수를 모듈의 기본 내보내기로 설정하여 다른 파일에서 사용할 수 있게 한다.
 module.exports = connect;
